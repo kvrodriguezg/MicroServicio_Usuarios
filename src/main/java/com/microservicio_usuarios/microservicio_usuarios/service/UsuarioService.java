09a9e308f56c89c3ada40adeed6bd7ff68fffb05
Katlheen Rodriguez <kv.rodriguezgarcia@gmail.com>
2025-11-26 01:53:29 -0300

    ajustes

--- src/main/java/com/microservicio_usuarios/microservicio_usuarios/service/UsuarioService.java
+++ src/main/java/com/microservicio_usuarios/microservicio_usuarios/service/UsuarioService.java
@@ -19,13 +19,13 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
 
-    // Obtener todos los usuarios
+    //Obtener todos los usuarios
     public List<Usuario> listarTodos() {
         log.info("Listando todos los usuarios");
         return usuarioRepository.findAll();
     }
 
-    // Crear un usuario
+    //Crear un usuario
     public Usuario crear(Usuario usuario) {
         log.info("Creando usuario: {}", usuario.getEmail());
 
@@ -37,26 +37,28 @@ public class UsuarioService {
         usuario.setFechaRegistro(LocalDateTime.now());
         Usuario saved = usuarioRepository.save(usuario);
 
-        // Evita advertencia de tipo nulo
+        //Evita advertencia de tipo nulo
         Long id = Objects.requireNonNull(saved.getId(), "El id no debe ser nulo después de guardar");
         log.info("Usuario creado con id {}", id);
 
         return saved;
     }
 
-    // Obtener usuario mediante id
+    //Obtener usuario mediante id
+    @SuppressWarnings("null")
     public Optional<Usuario> obtenerUsuarioPorId(Long id) {
         log.info("Buscando usuario con id {}", id);
         return usuarioRepository.findById(id);
     }
 
-    // Obtener usuario mediante con excepción
+    //Obtener usuario mediante con excepción
+    @SuppressWarnings("null")
     private Usuario obtenerPorId(Long id) {
         return usuarioRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con id: " + id));
     }
 
-    // Actualizar un usuario
+    //Actualizar un usuario
     public Usuario actualizar(Long id, Usuario dto) {
         log.info("Actualizando usuario con id {}", id);
 
@@ -73,14 +75,15 @@ public class UsuarioService {
 
         Usuario updated = usuarioRepository.save(existente);
 
-        // Evita advertencia de tipo nulo
+        //Evita advertencia de tipo nulo
         Long updatedId = Objects.requireNonNull(updated.getId(), "El id no debe ser nulo después de actualizar");
         log.info("Usuario actualizado id {}", updatedId);
 
         return updated;
     }
 
-    // Eliminar usuario
+    //Eliminar usuario
+    @SuppressWarnings("null")
     public void eliminar(Long id) {
         log.info("Eliminando usuario con id {}", id);
         Usuario existente = obtenerPorId(id);
